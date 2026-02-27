d2d5584002328bbce8971e94a703cf3eaea76331
antxinerx119 <186919065+antxinerx119@users.noreply.github.com>
2026-02-27 01:19:24 +0100

    Fix merge conflicts: restore develop CMakeLists.txt, move registerwindow to client, fix login signal

--- client/src/front/loginwindow.cpp
+++ client/src/front/loginwindow.cpp
@@ -8,29 +8,19 @@ LoginWindow::LoginWindow(QWidget *parent)
 {
     usernameInput = new QLineEdit;
     usernameInput->setPlaceholderText("Nom d'utilisateur");
-<<<<<<< HEAD:client/src/front/loginwindow.cpp
+    
     passwordInput = new QLineEdit;
     passwordInput->setPlaceholderText("Mot de passe");
     passwordInput->setEchoMode(QLineEdit::Password);
-=======
-    //à ajouter une fois que vous pouvez integrer le mot de passe
-    //passwordInput = new QLineEdit;
-    //passwordInput->setPlaceholderText("Mot de passe");
->>>>>>> origin/Front:loginwindow.cpp
 
     loginButton = new QPushButton("Se connecter");
     registerButton = new QPushButton("Inscription");
+    
     auto layout = new QVBoxLayout;
-<<<<<<< HEAD:client/src/front/loginwindow.cpp
     layout->addWidget(usernameInput);
     layout->addWidget(passwordInput);
     layout->addWidget(loginButton);
-=======
-    layout ->addWidget(usernameInput);
-    //layout ->addWidget(passwordInput);
-    layout ->addWidget(loginButton);
-    layout ->addWidget(registerButton);
->>>>>>> origin/Front:loginwindow.cpp
+    layout->addWidget(registerButton);
     setLayout(layout);
 
     connect(loginButton, &QPushButton::clicked, this, [this](){
--- client/src/front/loginwindow.h
+++ client/src/front/loginwindow.h
@@ -15,14 +15,8 @@ public:
     explicit LoginWindow(QWidget *parent = nullptr);
 
 signals:
-<<<<<<< HEAD:client/src/front/loginwindow.h
     void loginRequested(QString username, QString password);
-=======
-    void loginRequested(QString username);
     void registerRequested();
-    //à ajouter une fois le mot de passe opérationnel
-    //void loginRequested(QString username, QString motdepasse);
->>>>>>> origin/Front:loginwindow.h
 
 private:
     QLineEdit *usernameInput;
